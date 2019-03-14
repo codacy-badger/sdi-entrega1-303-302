f31fce6b7c3303749ec22acd8387af087c929536
yagogr98 <yagogr98@gmail.com>
2019-03-14 17:22:04 +0100

    Añadidas clases de test

---
+++ sdi-entrega1-303-302/src/test/java/com/uniovi/tests/pageobjects/PO_HomeView.java
@@ -0,0 +1,61 @@
+package com.uniovi.tests.pageobjects;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import com.uniovi.tests.util.SeleniumUtils;
+
+public class PO_HomeView extends PO_NavView {
+
+	private WebDriver driver;
+
+	static public void checkWelcome(WebDriver driver, int language) {
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("welcome.message", language), getTimeout());
+
+	}
+
+	static public void checkChangeIdiom(WebDriver driver, String textIdiom1, String textIdiom2, int locale1,
+			int locale2) {
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+		// Cambiamos a segundo idioma
+		PO_HomeView.changeIdiom(driver, textIdiom2);
+		// COmprobamos que el texto de bienvenida haya cambiado a segundo idioma
+		PO_HomeView.checkWelcome(driver, locale2);
+		// Volvemos a Español.
+		PO_HomeView.changeIdiom(driver, textIdiom1);
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+
+	}
+
+	// PR01. Acceder a la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-­‐primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-­‐primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
+}
